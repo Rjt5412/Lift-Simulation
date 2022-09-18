13b06973817243e43e9f6afe1615d90d2950b83f
Rajat Malviya <rajat@RajatM1.local>
2022-09-18 13:42:51 +0530

    Added lifts with styling and some minor fixes #1

--- src/js/main.js
+++ src/js/main.js
@@ -12,10 +12,10 @@ function handleFormSubmit(event){
     const form_data = new FormData(event.target);
     for (const [key, value] of form_data.entries()){
         if (key==="no-of-floors"){
-            numOfFloors = parseInt(value, 10)
+            numOfFloors = parseInt(value, 10);
         }
         if (key==="no-of-lifts"){
-            numOfLifts = parseInt(value, 10)
+            numOfLifts = parseInt(value, 10);
         }
     }
 
@@ -28,42 +28,64 @@ function handleFormSubmit(event){
 
 function initializeApp(){
 
-    setFloors();
-    setLifts();
+    createFloors();
+    createLifts();
 }
 
-function setFloors(){
-    for (let i=0; i<numOfFloors; i++){
-        // Create new floor
-        const newFloor = document.createElement("div")
-        newFloor.className = "floor";
-
-        // Add this floor to the lift-space
-        liftsSpace.prepend(newFloor)
-
-        // Add the up and down buttons
-        const buttons = document.createElement("div")
-        buttons.className = "buttons"
-
-        const buttonUp = document.createElement("button")
-        buttonUp.className = "button-up"
-        buttonUp.innerText = "UP"
-
-        const buttonDown = document.createElement("button")
-        buttonDown.className = "button-down"
-        buttonDown.innerText = "DOWN"
-
-        buttons.append(buttonUp, buttonDown)
-
-        // Add the floor number
-        const floorNum = document.createElement("div")
-        floorNum.className = "floor-num-text"
-        floorNum.innerText = `Floor${i}`
-        
-        newFloor.append(buttons, floorNum)
+function createFloors(){
+    if (numOfFloors && numOfFloors>=0){
+        for (let i=0; i<numOfFloors; i++){
+            // Create new floor
+            const newFloor = document.createElement("div");
+            newFloor.className = "floor";
+    
+            // Add this floor to the lift-space
+            liftsSpace.prepend(newFloor);
+    
+            // Add the up and down buttons
+            const buttons = document.createElement("div");
+            buttons.className = "buttons";
+    
+            const buttonUp = document.createElement("button");
+            buttonUp.className = "button-up";
+            buttonUp.innerText = "UP";
+    
+            const buttonDown = document.createElement("button");
+            buttonDown.className = "button-down";
+            buttonDown.innerText = "DOWN";
+    
+            buttons.append(buttonUp, buttonDown);
+    
+            // Add the floor number
+            const floorNum = document.createElement("div");
+            floorNum.className = "floor-num-text";
+            floorNum.innerText = `Floor${i}`;
+            
+            newFloor.append(buttons, floorNum);
+        }
     }
+    
 }
 
-function setLifts() {
+function createLifts() {
+    if (numOfLifts && numOfLifts >=0){
+        liftsArea = document.createElement("div");
+        liftsArea.className = "lifts-area";
+        for(let i=0; i<numOfLifts; i++){
+            lift = document.createElement("div");
+            lift.className = "lift";
 
+            // create left and right doors
+            left_door = document.createElement("div");
+            left_door.className = "left-door";
+            right_door = document.createElement("div");
+            right_door.className = "right-door";
+            lift.append(left_door, right_door);
+
+            liftsArea.append(lift);
+
+            // Place all the lifts starting from the bottom floor
+            liftsSpace.childNodes[numOfFloors-1].append(liftsArea)
+        }
+    }
 }
