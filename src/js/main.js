e70ccaefc62b4892514ba7a05888f5fc334f406f
Rajat Malviya <rajat@RajatM1.local>
2022-09-18 12:59:11 +0530

    Added logic to generate dumb and non styled UI based on user inputs #1

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
-form = document.getElementById("lift-simulation-form");
+const form = document.getElementById("lift-simulation-form");
+const liftsSpace = document.querySelector(".lifts-space")
 
-let numOfLifts = 1;
-let numOfFloors = 2;
+
+let numOfLifts = 1
+let numOfFloors = 2
 
 form.addEventListener("submit", handleFormSubmit)
 
@@ -13,10 +15,55 @@ function handleFormSubmit(event){
             numOfFloors = parseInt(value, 10)
         }
         if (key==="no-of-lifts"){
-            numOfFloors = parseInt(value, 10)
+            numOfLifts = parseInt(value, 10)
         }
     }
 
-    // console.log(`Floors: ${numOfFloors}`)
-    // console.log(`Lifts: ${numOfLifts}`)
+    // console.log(`Floors: ${numOfFloors}`);
+    // console.log(`Lifts: ${numOfLifts}`);
+
+    initializeApp();
+
+}
+
+function initializeApp(){
+
+    setFloors();
+    setLifts();
+}
+
+function setFloors(){
+    for (let i=0; i<numOfFloors; i++){
+        // Create new floor
+        const newFloor = document.createElement("div")
+        newFloor.className = "floor";
+
+        // Add this floor to the lift-space
+        liftsSpace.prepend(newFloor)
+
+        // Add the up and down buttons
+        const buttons = document.createElement("div")
+        buttons.className = "buttons"
+
+        const buttonUp = document.createElement("button")
+        buttonUp.className = "button-up"
+        buttonUp.innerText = "UP"
+
+        const buttonDown = document.createElement("button")
+        buttonDown.className = "button-down"
+        buttonDown.innerText = "DOWN"
+
+        buttons.append(buttonUp, buttonDown)
+
+        // Add the floor number
+        const floorNum = document.createElement("div")
+        floorNum.className = "floor-num-text"
+        floorNum.innerText = `Floor${i}`
+        
+        newFloor.append(buttons, floorNum)
+    }
+}
+
+function setLifts() {
+
 }
